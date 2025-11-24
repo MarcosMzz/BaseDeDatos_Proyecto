8b3f234661125991e3e0bb6eeae1920fa9004152
Marcos Mazzanti <marcossantinomazzanti@gmail.com>
2025-11-24 00:25:18 -0300

    Guia de como utilizar los archivos generados incluida
    Added comments for data insertion and permissions setup.

--- ScriptBDD/Tema2_OptimizacionPorIndiice/generarDatos.py
+++ ScriptBDD/Tema2_OptimizacionPorIndiice/generarDatos.py
@@ -41,3 +41,38 @@ with open('examenes_prueba.csv', 'w', newline='', encoding='utf-8') as f:
         writer.writerow([fecha.isoformat(), id_materia])
 
 print(f"Archivo examenes_prueba.csv generado con {CANTIDAD_REGISTROS} registros.")
+
+# Para insertar los datos generados se recomienda utilziar una view de las tablas Materia y Examen debido
+# que hace conflicto con la primaryKey + Identity
+#A continuacion dejo ambos ejemplos
+# View para materia
+#CREATE VIEW dbo.v_materia_bulk AS
+#SELECT
+#   nombre_materia,
+#   id_cuatri,
+#   anio_cursada
+#FROM
+#   materia;
+
+# View para Examen
+#CREATE VIEW dbo.v_examen_bulk AS
+#SELECT
+#    fecha,
+#    id_materia
+#FROM
+#   examen;
+
+#Ademas se debe crear una carpeta en la raiz del disco C llamada datos y darle los permisos al sql.
+#El nombre es opcional pero de esa forma ya se puede utilizar la prueba de indice sin cambiar nada.
+#Como dar permisos 
+#Carpeta que contiene los archivos cvs
+# -> Propiedades
+# -> Seguridad
+# -> Editar
+# -> Agregar
+# -> NT SERVICE\MSSQL$SQLEXPRESS
+# -> Comprobar nombres
+# -> Aceptar
+# -> Permisos: Lectura / Ejecución
+# -> Aplicar
+# -> OK
